7beb579a757b9465160b370e817bfd4b82546222
Joao Leonardo Pereira <joaoleonardo_pereira@apple.com>
2023-01-12 07:31:19 -0300

    add angularfire

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    firebase: {
+        projectId: '<YOUPROJECTID>',
+        appId: '<YOUAPPID>',
+        storageBucket: '<YOURBUCKET>.appspot.com',
+        locationId: '<YOURLOCATION>',
+        apiKey: '<APIKEY>',
+        authDomain: '<AUTHDOMAIN>.firebaseapp.com',
+        messagingSenderId: '<SENDERID>',
+        measurementId: 'G-<MEASUREMENTID>',
+    },
+    production: false
+}
